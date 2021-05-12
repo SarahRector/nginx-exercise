abbeb486aed1eee2842d8db1c5df8a230d1d473c
SarahRector <rector.sarah.r@gmail.com>
2021-05-12 16:57:58 -0700

    read file working

--- reader.js
+++ reader.js
@@ -1,3 +1,9 @@
 #!/usr/bin/env node
 
-console.log("what a reader")
+const fs = require("fs");
+
+const readFile = process.argv[2];
+
+const file = fs.readFileSync(readFile, "utf-8");
+
+console.log(file.split('\n'));
